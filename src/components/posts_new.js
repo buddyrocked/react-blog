beab7c674708cac3e39f250382f0426c0b399e8f
budihariyana <budihariyana@gmail.com>
2018-01-10 06:16:51 +0700

    handle submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,34 +12,64 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+        { field.meta.touched ? field.meta.error : '' }
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div>
         <Link className="btn btn-primary" to="/">
           Back To List
         </Link>
         <hr />
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field
             label="Title"
             name="title"
             component={this.renderField}
           />
           <Field
-            label="Tags"
-            name="tags"
+            label="Categories"
+            name="categories"
             component={this.renderField}
           />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+          <button type="submit" className="btn btn-primary">submit</button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values){
+  const errors = {};
+
+  if(!values.title){
+    errors.title = 'enter title';
+  }
+  if(!values.categories){
+    errors.categories = 'enter categories';
+  }
+  if(!values.content){
+    errors.content = 'enter content';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 }) (PostsNew);
