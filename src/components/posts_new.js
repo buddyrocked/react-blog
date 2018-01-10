8d41f651bfd91a5a81c4b82a3b7d5bc9aadfe992
Budi Hariyana <budihariyana@gmail.com>
2018-01-10 15:45:36 +0700

    error form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,15 +4,20 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
+    const  { meta : { touched, error } } = field;
+    const className = `form-group ${ touched && error ? 'has-danger' : '' }`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        { field.meta.touched ? field.meta.error : '' }
+        <div className="text-help">
+          { touched ? error : '' }
+        </div>
       </div>
     );
   }
@@ -47,6 +52,9 @@ class PostsNew extends Component {
             component={this.renderField}
           />
           <button type="submit" className="btn btn-primary">submit</button>
+          <Link className="btn btn-danger" to="/">
+            Cancel
+          </Link>
         </form>
       </div>
     );
