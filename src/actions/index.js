540cf389b784d54dbfc69b4502a5a3212559240d
Budi Hariyana <budihariyana@gmail.com>
2018-01-11 08:26:34 +0700

    callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
-export cosnt CREATE_POST = 'create_post';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP1234';
@@ -15,8 +15,9 @@ export function fetchPosts() {
   };
 }
 
-export function createPost(values){
-  const request = axios.post(`${ROOT_URL}/posts/${API_KEY}`, values);
+export function createPost(values, callback){
+  const request = axios.post(`${ROOT_URL}/posts/${API_KEY}`, values)
+    .then(()=>callback());
 
   return {
     type: CREATE_POST,
