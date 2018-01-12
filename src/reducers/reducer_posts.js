001e5ce84de01266f70f972310b36791b79e2540
Budi Hariyana <budihariyana@gmail.com>
2018-01-12 08:59:12 +0700

    post show reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,8 +4,11 @@ import { FETCH_POSTS, FETCH_POST } from '../actions';
 export default function(state = {}, action){
   switch(action.type){
   case FETCH_POST:
+    //const post = action.payload.data;
+    //const newState { ...state };
+    //return newState[post.id] = post;
 
-    return { ...state, post };
+    return { ...state, [action.payload.data.id] : action.payload.data };
   case FETCH_POSTS:
     console.log(action.payload.data);
     return _.mapKeys(action.payload.data, 'id');
